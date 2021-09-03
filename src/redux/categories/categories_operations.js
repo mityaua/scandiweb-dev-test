e010b4340739a67d40ba7bf69ec9ed6df76a2f96
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-09-03 14:39:30 +0300

    Removed comments

--- src/redux/categories/categories_operations.js
+++ src/redux/categories/categories_operations.js
@@ -2,9 +2,8 @@ import {
   getAllCategoriesRequest,
   getAllCategoriesSuccess,
   getAllCategoriesError,
-} from './categories_action'; // Импорт экшенов из контактов в операции
+} from './categories_action';
 
-// Операция получения информации о юзере
 export const getCategories = array => async dispatch => {
   console.log('Hello!');
 
